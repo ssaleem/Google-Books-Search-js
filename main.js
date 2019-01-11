019b1f52d736e1a7df300fd3ef337fdafca3656c
Sara Saleem <8222963+ssaleem@users.noreply.github.com>
2019-01-11 14:45:29 -0500

    Add basic search
    Add event listeners for search bar and perform basic search

--- main.js
+++ main.js
@@ -1,13 +1,48 @@
+const search = {
+  init(){
+    this.searchBar = document.querySelector('#search-bar');
+    this.searchBtn = document.querySelector('#search-btn');
+
+    // Search bar listeners
+    this.searchBar.addEventListener('keypress', (event) => {
+      (event.keyCode === 13 && event.target.value !== '') && controller.searchForBooks(event.target.value);
+    });
+
+    this.searchBtn.addEventListener('click', () => {
+      (searchBar.value !== '') && controller.searchForBooks(searchBar.value);
+    });
+  }
+}
+
+const controller = {
+  init() {
+    search.init();
+  },
+  // Searches for books and returns a promise that resolves a JSON list
+  searchForBooks(term) {
+    const GBOOKs_API_KEY = 'AIzaSyCZ2yqlOHMVHhoistTvvHWP542UlgmvJrU';
+    const gBooksApiURL = `https://www.googleapis.com/books/v1/volumes?key=${GBOOKs_API_KEY}&q=`;
+    return fetch(`${gBooksApiURL}${term}`)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      else {
+        throw new Error('Sorry something went wrong with Google Books API');
+      }
+    });
+  }
+}
+
+controller.init();
+
 // Renders an error message
 function showError(msg) {
   const html = `<li><p class="error">${msg}</p></li>`;
   document.querySelector('#results').innerHTML = html;
 }
 
-// Searches for books and returns a promise that resolves a JSON list
-function searchForBooks(term) {
-  // TODO
-}
+
 
 // Generate HTML and sets #results's contents to it
 function render() {
